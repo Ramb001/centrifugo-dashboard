99d8d98fd510b859d3e8843c6457adb47dffcede
Ramb001 <artem.rmb@bk.ru>
2025-01-06 16:07:54 +0300

    fix: fix

--- tg-bot/handlers/onboarding.py
+++ tg-bot/handlers/onboarding.py
@@ -12,16 +12,23 @@ async def onboarding(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_chat_action(update.effective_chat.id, ChatAction.TYPING)
 
     async with aiohttp.ClientSession() as client:
-        user = await PB.add_record(
+        user_ = await PB.fetch_records(
             PocketbaseCollections.USERS,
             client,
-            tg_id=update.effective_user.id,
-            username=update.effective_user.username,
-            chat_id=update.effective_chat.id,
-            jwt=generate_jwt_token(update.effective_user.id),
+            filter=f"tg_id=(tg_id='{update.effective_user.id}')",
         )
 
-        await CENTRIFUGO.subscribe(client, "general", user["jwt"], {})
+        if len(user_["items"]) == 0:
+            user = await PB.add_record(
+                PocketbaseCollections.USERS,
+                client,
+                tg_id=update.effective_user.id,
+                username=update.effective_user.username,
+                chat_id=update.effective_chat.id,
+                jwt=generate_jwt_token(update.effective_user.id),
+            )
+
+            await CENTRIFUGO.subscribe(client, "general", user["jwt"], {})
 
     await update.message.reply_text(
         BotReplies.ON_BOARDING,
